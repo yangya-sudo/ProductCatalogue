72a0701d9da032335be5a9c0ac2bdae1c4b98d3b
yangya-sudo <yangyaprakashsatpathy@gmail.com>
2025-09-13 21:43:19 +0530

    fixed parent child POM relation

---
+++ src/main/java/org/example/controller/ProductOfferingApiImplementation.java
@@ -0,0 +1,46 @@
+package src.main.java.org.example.controller;
+
+import org.generated.tmfapi.controller.ProductOfferingApi;
+import org.generated.tmfapi.model.ProductOffering;
+import org.generated.tmfapi.model.ProductOfferingFVO;
+import org.generated.tmfapi.model.ProductOfferingMVO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductOfferingApiImplementation implements ProductOfferingApi {
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return ProductOfferingApi.super.getRequest();
+    }
+
+    @Override
+    public ResponseEntity<ProductOffering> createProductOffering(ProductOfferingFVO productOfferingFVO, String fields) {
+        return ProductOfferingApi.super.createProductOffering(productOfferingFVO, fields);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteProductOffering(String id) {
+        return ProductOfferingApi.super.deleteProductOffering(id);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductOffering>> listProductOffering(String fields, Integer offset, Integer limit) {
+        return ProductOfferingApi.super.listProductOffering(fields, offset, limit);
+    }
+
+    @Override
+    public ResponseEntity<ProductOffering> patchProductOffering(String id, ProductOfferingMVO productOfferingMVO, String fields) {
+        return ProductOfferingApi.super.patchProductOffering(id, productOfferingMVO, fields);
+    }
+
+    @Override
+    public ResponseEntity<ProductOffering> retrieveProductOffering(String id, String fields) {
+        return ProductOfferingApi.super.retrieveProductOffering(id, fields);
+    }
+}
